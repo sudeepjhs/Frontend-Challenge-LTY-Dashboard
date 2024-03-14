cfd3601aeab6d7aa0ad679d3eae2c098f51f48bc
Sudeep Agarwal <sudeepagarwaljhs@gmail.com>
2024-03-15 00:35:11 +0530

    "added font"

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,4 +1,13 @@
 import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
+import localFont from 'next/font/local'
+
+export const fontAeonik = localFont({
+  src: [{
+    path: "./Aeonik.woff2"
+  }, {
+    path: "./Aeonik.woff",
+  }]
+})
 
 export const fontSans = FontSans({
   subsets: ["latin"],
